f492c8f58efc3a604cb94cced627b984f84af4a3
Stu <stumac1000@yahoo.com>
2023-11-03 17:25:35 -0700

    Style initial mobile menu

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
 import './normalize.css';
 import './style.css';
+
+window.addEventListener('resize', () => {
+  let w = window.innerWidth;
+  console.log('w');
+  console.log(w);
+  let h = window.innerHeight;
+  console.log('h');
+  console.log(h);
+  if (w <= 400) {
+    console.log('hellz ya');
+  }
+});
